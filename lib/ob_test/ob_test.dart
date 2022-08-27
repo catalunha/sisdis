27dca8f5abf4b9210846589549541e36e853cd59
catalunha <catalunha.mj@gmail.com>
2022-08-27 08:06:30 -0300

    obox ok.

--- lib/ob_test/ob_test.dart
+++ lib/ob_test/ob_test.dart
@@ -1,29 +1,35 @@
 // ignore_for_file: non_constant_identifier_names
 
-import 'dart:convert';
-import 'dart:io';
-
 import 'package:sisdis/ob_test/snis_model.dart';
 import 'package:sisdis/objectbox_models/objectbox.g.dart';
 
 ob_test() async {
   print('Start test');
   final store = openStore(directory: 'lib/objectbox_models');
-  final SNISModelBox = store.box<SNISModel>();
-  String dataMGFile = '/home/catalunha/myapp/sisdis/lib/data/mg/data_mg.json';
-  var dataMGJson = File(dataMGFile).readAsStringSync();
+  final SNISOBox = store.box<SNISModel>();
+  // String dataMGFile = '/home/catalunha/myapp/sisdis/lib/data/mg/data_mg.json';
+  // var dataMGJson = File(dataMGFile).readAsStringSync();
 
-  final dataMGJsonObj = json.decode(dataMGJson);
-  final List<SNISModel> list = [];
+  // final dataMGJsonObj = json.decode(dataMGJson);
+  // final List<SNISModel> list = [];
 
-  list.clear();
-  for (var element in dataMGJsonObj) {
-    list.add(SNISModel.fromMap(element));
-  }
+  // list.clear();
+  // for (var element in dataMGJsonObj) {
+  //   list.add(SNISModel.fromMap(element));
+  // }
 
-  SNISModelBox.putMany(list);
-  List<SNISModel> SNISList = SNISModelBox.getAll();
+  // SNISOBox.putMany(list);
+  List<SNISModel> SNISList = SNISOBox.getAll();
   print(SNISList.length);
+
+  Query<SNISModel> query =
+      SNISOBox.query(SNISModel_.munCode.equals('310030')).build();
+  List<SNISModel> queryList = query.find();
+  print(queryList.length);
+
+  // SNISOBox.removeAll();
+
   store.close(); // don't forget to close the store
+
   print('End test');
 }
